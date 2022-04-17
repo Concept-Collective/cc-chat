7fb822b5e9df4b32d2176b21a7b7f757fd3746a8
Jye <jye1234567890@gmail.com>
2022-04-17 18:31:34 +0800

    Added version checker

--- server/main.lua
+++ server/main.lua
@@ -1,9 +1,15 @@
-ccChat = {}
-
 AddEventHandler('onResourceStart', function(resourceName)
     if (GetCurrentResourceName() ~= resourceName) then
         return
     end
-    --TODO CHECK FOR UPDATE
+    PerformHttpRequest('https://api.github.com/repos/Concept-Collective/cc-chat/releases/latest', function (err, data, headers)
+        local data = json.decode(data)
+        if data.tag_name ~= 'v'..GetResourceMetadata(GetCurrentResourceName(), 'version', 0)..'' then
+            print('^1================^0')
+            print('^1CC Chat ('..GetCurrentResourceName()..') is outdated!^0')
+            print('Current version: (^1v'..GetResourceMetadata(GetCurrentResourceName(), 'version', 0)..'^0)')
+            print('Latest version: (^2'..data.tag_name..'^0) '..data.html_url..'')
+            print('^1================^0')
+        end
+    end, 'GET', '')
 end)
-  
