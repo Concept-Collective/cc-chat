4854e6a3528d4b2156abe1e479771b49e8884aad
Mr. K V <36492979+69u@users.noreply.github.com>
2022-12-31 23:22:51 +0800

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,4 +1,4 @@
-version '1.2.8'
+version '1.2.9'
 author 'Concept Collective'
 description 'A chat theme for FiveM'
 
@@ -16,6 +16,10 @@ chat_theme 'ccChat' {
     msgTemplates = {
         ccChat = '<div id="notification" class="noisy"><div id="color-box" style="background-color: {0} !important;" class="noisy"></div><div id="info"><div id="top-info"><div id="left-info"><h1 id="title"><i class="{1}"></i></h1><h2 id="sub-title">{2}</h2></div><h2 id="time">{3}</h2></div><div id="bottom-info"><br><p id="text">{4}</p></div></div>'
     }
+}
+
+exports {
+    'getTimestamp'
 }
 
 game 'common'
