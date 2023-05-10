ef408ae8aaff5b647d3c8a7779b8cef685c6cd69
Jye <jye1234567890@gmail.com>
2023-05-10 21:01:51 +0800

    Added anti spam system

--- server/main.lua
+++ server/main.lua
@@ -26,3 +26,32 @@ AddEventHandler('onResourceStart', function(resourceName)
         end, 'GET', '')
     end
 end)
+
+-- Antispam System (Beta)
+local users = {}
+function checkSpam(source, message)
+    local BlockedStatus = false
+
+    -- Checks if the user has sent a message before
+    if users[source] == nil then
+        users[source] = {time = os.time()}
+        return false
+    end
+
+    -- Check if the message is a repeat of the last message
+    if message == users[source].lastMessage then
+        BlockedStatus = true
+    end
+
+    -- Check if the user has sent messages too quickly
+    if os.time() - users[source].time < 5 then
+        BlockedStatus = true
+    end
+
+    -- Update the user's information in the table
+    users[source] = {lastMessage = message, time = os.time()}
+
+    return BlockedStatus
+end
+
+exports('checkSpam', checkSpam)
