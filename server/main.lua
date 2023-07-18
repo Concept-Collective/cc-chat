f613f9ffd0cafa7fc5f9831e62d01c005875fa3a
Jye <jye1234567890@gmail.com>
2023-07-18 19:43:32 +0800

    Added a proper error message for version checking issue

--- server/main.lua
+++ server/main.lua
@@ -14,6 +14,11 @@ AddEventHandler('onResourceStart', function(resourceName)
     end
     if svConfig.versionChecker == true then
         PerformHttpRequest('https://api.github.com/repos/Concept-Collective/cc-chat/releases/latest', function (err, data, headers)
+            if data == nil then 
+                print('An error occurred while checking the version. Your firewall may be blocking access to "github.com". Please check your firewall settings and ensure that "github.com" is allowed to establish connections.')
+                return
+            end
+
             local data = json.decode(data)
             if data.tag_name ~= 'v'..GetResourceMetadata(GetCurrentResourceName(), 'version', 0) then
                 print('\n^1================^0')
